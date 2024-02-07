906683aaf42a2b71e64fc109ea0c935ce1d392f8
Jekies <jericho.kamantigue@gmail.com>
2024-02-07 22:52:57 +0800

    new files

--- src/assets/components/Cards.jsx
+++ src/assets/components/Cards.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-// import Single from '../assets/single.png'
-// import Double from '../assets/double.png'
-// import Triple from '../assets/triple.png'
+import Single from '../assets/single.png'
+import Double from '../assets/double.png'
+import Triple from '../assets/triple.png'
 
 const Cards = () => {
   return (
